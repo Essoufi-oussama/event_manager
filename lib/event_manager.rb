ffccdd5f0bde4df5a84cc2acca17160a745aa801
Essoufi-oussama <87605323+Essoufi-oussama@users.noreply.github.com>
2024-02-15 22:05:02 +0100

    moved zipcode check to method

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -7,9 +7,7 @@ contents = CSV.open(
   header_converters: :symbol
 )
 
-contents.each do |row|
-  name = row[:first_name]
-  zipcode = row[:zipcode]
+def clean_zipcode(zipcode)
   if zipcode.nil?
     zipcode = '00000'
   elsif zipcode.size < 5
@@ -17,5 +15,10 @@ contents.each do |row|
   elsif zipcode.size > 5
     zipcode.truncate(5)
   end
+end
+
+contents.each do |row|
+  name = row[:first_name]
+  zipcode = clean_zipcode(row[:zipcode])
   puts "#{name} #{zipcode}"
 end
